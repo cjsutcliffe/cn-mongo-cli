f7e7f24a342f56eb89688d7ae35b6324b8002dd7
Chris Sutcliffe <cjsutcliffe@gmail.com>
2023-01-10 16:57:04 +0000

    factorised index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,14 +12,6 @@ class Movie {
         await movieCollection.insertOne(this);
     };
    
-    async delete (movieCollection) {
-        console.log("Entering remove within index.js");
-        //code to save a movie to the database here;
-        await movieCollection.deleteOne(this);
-    };
-
-
-
 };
 
 module.exports = Movie;
